8c65da4f927098f2635078f8271db592238dc9b8
ismilearefin <arefinismile@gmail.com>
2023-02-10 11:53:15 +0600

    Discount section

---
+++ src/components/Home/DiscountSec/DiscountSec.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import discountImg from '../../../assets/discountImg.png';
+
+const DiscountSec = () => {
+    return (
+        <div style={{backgroundImage: `url(${discountImg})`}} className='w-[1920px] h-[418px]'>
+            
+        </div>
+    );
+};
+
+export default DiscountSec;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Collection from './Collection/Collection';
+import DiscountSec from './DiscountSec/DiscountSec';
 import HeroSection from './HoreSection/HeroSection';
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
         <div>
             <HeroSection></HeroSection>
             <Collection></Collection>
+            <DiscountSec></DiscountSec>
         </div>
     );
 };
